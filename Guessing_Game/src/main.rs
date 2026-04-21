8b2c85c8decbd36aea4a1ac5ba100fe091e28e6a
3mrkhalid <amrahmedyoussef100@gmail.com>
2026-04-21 23:35:54 +0200

    Refactor guessing game to use u8 for guesses

--- Guessing_Game/src/main.rs
+++ Guessing_Game/src/main.rs
@@ -5,21 +5,28 @@ use std::cmp::Ordering;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
+    let secret_number: u8 = rand::thread_rng().gen_range(1..=100);
 
     println!("Please input your guess.");
 
-    let mut guess: String = String::new();
+    loop {
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line!");
+        let mut guess: String = String::new();
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        let guess: u8 = guess.trim().parse().expect("Please type a number!");
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
+
 }
