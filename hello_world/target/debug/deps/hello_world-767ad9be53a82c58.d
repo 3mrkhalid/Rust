b75265c898cf690448cfff38f12774cbfe3e79ee
3mrkhalid <amrahmedyoussef100@gmail.com>
2026-04-21 22:42:11 +0200

    Delete hello_world/target directory

--- hello_world/target/debug/deps/hello_world-767ad9be53a82c58.d
+++
@@ -1,5 +0,0 @@
-/home/amrkhalid/projects/hello_world/target/debug/deps/hello_world-767ad9be53a82c58.d: src/main.rs
-
-/home/amrkhalid/projects/hello_world/target/debug/deps/libhello_world-767ad9be53a82c58.rmeta: src/main.rs
-
-src/main.rs:
