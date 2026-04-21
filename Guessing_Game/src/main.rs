9cd95c5853c2a51a5c7d73e64be8eef15517f099
3mrkhalid <amrahmedyoussef100@gmail.com>
2026-04-21 23:07:26 +0200

    Refactor guessing game prompts and input handling

--- Guessing_Game/src/main.rs
+++ Guessing_Game/src/main.rs
@@ -1,14 +1,25 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
+    println!("Guess the number!");
 
-    println!("!Guess the number!");
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
-    println!("please Enter your number");
+    println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let mut guess: String = String::new();
 
-    io::stdin().read_line(&mut guess).expect("Faild to read line!");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line!");
 
-    println!("You guess: {guess}");
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
